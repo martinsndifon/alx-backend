dafe725ac8b256a8fc34cd8a4f0111b09eb32c2a
Martins Ndifon <martinsndifon@gmail.com>
2023-08-01 12:34:22 +0000

    update to task-2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -27,7 +27,7 @@ def get_locale():
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello():
     """prints hello world"""
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == "__main__":
