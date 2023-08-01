deaf16f02a727b138886bad84f0e226d5e3ed89e
Martins Ndifon <martinsndifon@gmail.com>
2023-08-01 11:14:50 +0000

    New update to task-1, using flask-babel v2

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,22 +2,23 @@
 """ALX SE flask app"""
 from flask import Flask, render_template
 from flask_babel import Babel
-from config import Config
+
+
+class Config:
+    """flask-babel config class"""
+    LANGUAGES = ["en", "fr"]
 
 app =  Flask(__name__)
 app.config.from_object(Config)
 
-
-def get_locale():
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
 # Initialize and set default config for babel
-babel = Babel(app, locale_selector=get_locale, default_timezone='UTC')
+babel = Babel(app, default_locale=Config.LANGUAGES[0], default_timezone='UTC')
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello():
     """prints hello world"""
     return render_template('1-index.html')
+
 
 if __name__ == "__main__":
     """main entry"""
--- 0x02-i18n/config.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""configuration for flask app"""
-
-
-class Config:
-    """configuration class for the flask app"""
-    LANGUAGES = ['en', 'fr']
