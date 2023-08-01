69f4f33d1e517e91cf1c479d75c41ab7e7b5b8a4
Martins Ndifon <martinsndifon@gmail.com>
2023-08-01 11:05:44 +0000

    update to task-0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,12 +2,14 @@
 """ALX SE flask app"""
 from flask import Flask, render_template
 
-app =  Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello():
     """prints hello world"""
     return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     """main entry"""
