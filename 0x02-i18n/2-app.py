0295a6ab329606ca67f99d7e81a3e1604a459ce9
Martins Ndifon <martinsndifon@gmail.com>
2023-08-01 11:33:28 +0000

    added doc for function get_locale

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -20,6 +20,7 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
+    """Determine the best match with supported languages"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
