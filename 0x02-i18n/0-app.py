bbfa87dc2c237dc1a00442f8deedf678af6e2448
Martins Ndifon <martinsndifon@gmail.com>
2023-08-01 10:59:37 +0000

    updatet to task-0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,7 +7,7 @@ app =  Flask(__name__)
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello():
     """prints hello world"""
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 if __name__ == "__main__":
     """main entry"""
