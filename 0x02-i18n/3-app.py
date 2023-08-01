bc0cff8231670e97504bee214aa4d8338cda3464
Martins Ndifon <martinsndifon@gmail.com>
2023-08-01 12:21:59 +0000

    Update to task-3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -21,8 +21,7 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale():
     """Determine the best match with supported languages"""
-    # return request.accept_languages.best_match(app.config['LANGUAGES'])
-    return 'fr'
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
