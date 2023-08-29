37ade4e41329c3b9da66e13b5616b4dcb40447e7
Martins Ndifon <martinsndifon@gmail.com>
2023-08-29 13:40:06 +0000

    update to the basic redis server

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -2,8 +2,9 @@ import { createClient } from 'redis';
 
 const client = createClient();
 
-client.on('error', (err) =>
-  console.log('Redis client not connected to the server:', err)
-);
-client.on('connect', () => console.log('Redis client connected to the server'));
+client.connect().then(() => {
+  console.log('Redis client connected to the server');
+}).catch((err) => {
+  console.log('Redis client not connected to the server:', err.message);
+})
 
